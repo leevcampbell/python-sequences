618e6833b23e2200a7fff7f83e2f5691aad1d87d
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-04-10 13:59:05 -0400

    passed all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a = 0
+    b = 1
+    fib_list = []
+    for i in range(length):
+        fib_list.append(a)
+        a, b = b, a + b
+    print(fib_list)
